bfc410074548544ffc78826e3f335ba5ee28137a
Renan Souza <renaninacio506@gmail.com>
2026-09-11 17:36:48 -0300

    feat(core): add main application class and tests

---
+++ src/main/java/com/aep/grinops/GrinOpsApplication.java
@@ -0,0 +1,12 @@
+package com.aep.grinops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrinOpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrinOpsApplication.class, args);
+    }
+}
